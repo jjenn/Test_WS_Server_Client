bb67f1a2f07d5dadef759c941723c9332a294620
jjenn <jenny.g@mutexcrew.com>
2021-06-04 18:19:23 +0700

    try using same wifi at home

--- websocket_client/WsClientExecutor.cs
+++ websocket_client/WsClientExecutor.cs
@@ -4,7 +4,7 @@ namespace Mutex.MVC.Multiplayer
 {
     public class WsClientExecutor
     {
-        private static readonly string uri = "ws://localhost/wsListener";
+        private static readonly string uri = "ws://192.168.1.113:8080/wsListener";
 
         public static void Main(string[] args)
         {
--- websocket_host/WsServerExecutor.cs
+++ websocket_host/WsServerExecutor.cs
@@ -4,7 +4,7 @@ namespace Mutex.MVC.Multiplayer
 {
     public class WsServerExecutor
     {
-        private static readonly string uri = "http://+:80/wsListener/";
+        private static readonly string uri = "http://192.168.1.113:8080/wsListener/";
 
         public static void Main(string[] args)
         {
